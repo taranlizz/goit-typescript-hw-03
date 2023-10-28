268fcf5300b20849213c1fe64bb34fbc192fba6d
Elizaveta Taran <lizataran103@gmail.com>
2023-10-28 13:51:20 +0300

    fix

--- src/4.ts
+++ src/4.ts
@@ -6,9 +6,7 @@ class Key{
 }
 
 class Person{
-    constructor(private key: Key) {
-        this.key = key;
-    }
+    constructor(private key: Key) {}
     getKey() {
     return this.key
     }
@@ -18,9 +16,7 @@ abstract class House{
     protected door = false;
     private tenants: Person[] = [];
 
-    constructor(protected key: Key) {
-        this.key = key
-    }
+    constructor(protected key: Key) {}
 
     comeIn(person: Person) {
         if (this.door) {
